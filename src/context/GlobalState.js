eb63c5127120cb1ea0e3f75f5bce97d3f457e940
Muhammad Tahir Musharraf <43595126+Tahir-Musharraf@users.noreply.github.com>
2022-08-11 10:55:35 +0500

    Update: now add transcation function is working

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,12 +1,7 @@
 import react, { useReducer, createContext } from 'react';
 import AppReducer from './AppReducer';
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-]
+    transactions: []
 }
 
 
@@ -32,7 +27,8 @@ export const GlobalProvider = ({ children }) => {
 
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>);
