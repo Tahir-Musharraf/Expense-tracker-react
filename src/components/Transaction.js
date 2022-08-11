62cbbc79de53b979f91b8c3d93f5a6d78bf2cbb0
Muhammad Tahir Musharraf <43595126+Tahir-Musharraf@users.noreply.github.com>
2022-08-11 10:44:38 +0500

    Update: transaction removal function working

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,5 @@
 import React, {useContext}from 'react'
-import { GlobalContext  } from '../context/GlobalState'
+import {  GlobalContext  } from '../context/GlobalState'
 
 //Money formatter function
 function moneyFormatter(num) {
@@ -22,7 +22,7 @@ export const Transaction = ({ transaction}) => {
   const sign = transaction.amount < 0 ? '-' : '+';
   return (
     <li className={transaction.amount < 0 ? 'minus':'plus'}>
-          { transaction.text } <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+          { transaction.text } <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
     </li>
   )
 }
